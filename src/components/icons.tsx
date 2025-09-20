85ace8ec12dba59003ee746f4ad2606b4787aaa6
Dhruv Soran <dhruvsoran@gmail.com>
2025-09-20 16:03:45 +0000

    can you change the kalaconnect logo

--- src/components/icons.tsx
+++ src/components/icons.tsx
@@ -5,10 +5,21 @@ export function KalaConnectIcon(props: SVGProps<SVGSVGElement>) {
     <svg
       xmlns="http://www.w3.org/2000/svg"
       viewBox="0 0 24 24"
-      fill="currentColor"
+      fill="none"
+      stroke="currentColor"
+      strokeWidth="2"
+      strokeLinecap="round"
+      strokeLinejoin="round"
       {...props}
     >
-      <path d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm4.24 14.76L12 12.52l-4.24 4.24-1.41-1.41L10.59 12 6.34 7.76l1.41-1.41L12 10.1l4.24-4.24 1.41 1.41L13.41 12l4.24 4.24-1.41 1.41zM12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm0 18c-4.41 0-8-3.59-8-8s3.59-8 8-8 8 3.59 8 8-3.59 8-8 8zm-2-8l-4-4v8l4-4zm4 0l4-4v8l-4-4z" />
+      <path d="M12 2L12 2A4.5 4.5 0 0 1 16.5 6.5L16.5 6.5" />
+      <path d="M12 2L12 2A4.5 4.5 0 0 0 7.5 6.5L7.5 6.5" />
+      <path d="M2 12L2 12A4.5 4.5 0 0 1 6.5 7.5L6.5 7.5" />
+      <path d="M2 12L2 12A4.5 4.5 0 0 0 6.5 16.5L6.5 16.5" />
+      <path d="M12 22L12 22A4.5 4.5 0 0 0 16.5 17.5L16.5 17.5" />
+      <path d="M12 22L12 22A4.5 4.5 0 0 1 7.5 17.5L7.5 17.5" />
+      <path d="M22 12L22 12A4.5 4.5 0 0 0 17.5 7.5L17.5 7.5" />
+      <path d="M22 12L22 12A4.5 4.5 0 0 1 17.5 16.5L17.5 16.5" />
     </svg>
   );
 }
