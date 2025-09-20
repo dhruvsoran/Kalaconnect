791c7964afec7ea372cc78f050a59e8352f53f07
Dhruv Soran <dhruvsoran@gmail.com>
2025-09-20 16:06:46 +0000

    also add somewhere in website as i developed this website that is Dhruv

--- src/components/site-footer.tsx
+++ src/components/site-footer.tsx
@@ -4,6 +4,7 @@ export function SiteFooter() {
         <footer className="bg-card border-t">
             <div className="container mx-auto py-8 px-4 sm:px-6 lg:px-8 text-center text-muted-foreground">
                 <p>&copy; {new Date().getFullYear()} KalaConnect. All rights reserved.</p>
+                <p className="text-sm mt-2">Developed by Dhruv Soran</p>
             </div>
         </footer>
     );
