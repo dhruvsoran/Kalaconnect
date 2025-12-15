a39b7c6c61466a22c2d80d409680cc464bb3a318
Dhruv Soran <dhruvsoran@gmail.com>
2025-12-15 23:34:35 +0530

    Add new image source to next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -23,6 +23,13 @@ const nextConfig: NextConfig = {
         port: '',
         pathname: '/**',
       },
+          {
+      protocol: 'https',
+      hostname: 'e7.pngegg.com',
+      port: '',
+      pathname: '/**',
+    },
+
       {
         protocol: 'https',
         hostname: 'm.media-amazon.com',
