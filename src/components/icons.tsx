a60920d43536f294707cc30555b05cc6a58dc040
Dhruv Soran <dhruvsoran@gmail.com>
2025-12-15 23:22:10 +0530

    Update KalaConnectIcon image source

--- src/components/icons.tsx
+++ src/components/icons.tsx
@@ -5,7 +5,7 @@ export function KalaConnectIcon(props: SVGProps<SVGSVGElement>) {
   const { className, ...rest } = props;
   return (
     <Image
-      src="https://thumbs.dreamstime.com/b/rainbow-circle-logo-logo-rainbow-circle-symbol-infinite-cycle-color-abstract-isolated-pattern-white-background-120099960.jpg"
+      src="https://e7.pngegg.com/pngimages/11/772/png-clipart-multicolored-woman-face-wall-art-art-graphic-design-creativity-artwork-creative-artwork-logo.png"
       alt="KalaConnect Logo"
       width={props.width ? Number(props.width) : 32}
       height={props.height ? Number(props.height) : 32}
